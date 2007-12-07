b30ac5e2309f825e4643e49fb0e9cf1c58a503fa
Travis Schmidt <travis.schmidt@gmail.com>
2007-12-07 15:23:45 +0000

    Upgraded TableWidget to be easier to use in and out of Screen Framework

---
+++ src/org/openelis/gwt/client/widget/table/TableWidget.java
@@ -0,0 +1,132 @@
+package org.openelis.gwt.client.widget.table;
+
+import com.google.gwt.i18n.client.ConstantsWithLookup;
+import com.google.gwt.user.client.ui.Composite;
+
+import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
+import org.openelis.gwt.common.AbstractField;
+
+import java.util.ArrayList;
+
+public class TableWidget extends Composite {
+
+    public TableController controller = new TableController();
+    public ConstantsWithLookup constants = null;
+    
+    public TableWidget() {
+        initWidget(controller.view);
+    }
+    /**
+     * Sets the height of the table.
+     */
+    public void setHeight(String height) {
+        controller.view.setHeight(height);
+    }
+
+    /**
+     * Sets the width of the table.
+     */
+    public void setWidth(String width) {
+        controller.view.setWidth(width);
+    }
+
+    /**
+     * Sets the TableManager for this table
+     * 
+     * @param manager
+     */
+    public void setManager(TableManager manager) {
+        controller.setManager(manager);
+    }
+
+    /**
+     * This method creates the editors that the table will use.
+     * 
+     * @param node
+     */
+    public void setEditors(TableCellWidget[] cells) {
+        controller.setEditors(cells);
+    }
+
+    /**
+     * This method will set the Model fields to be used by the table.
+     * 
+     * @param node
+     */
+    public void setFields(AbstractField[] fields) {
+
+        controller.model.setFields(fields);
+    }
+
+    /**
+     * This table will set the Column Aligns of the table.
+     * 
+     * @param node
+     */
+    public void setColAlign(HorizontalAlignmentConstant[] alignments) {
+
+        controller.setColAlign(alignments);
+    }
+
+    /**
+     * This Table will set any static filters defined for the table.
+     * 
+     * @param node
+     */
+    public void setStatFilter(ArrayList filters) {
+
+        controller.setStatFilterable(filters);
+    }
+
+    public void setTableTitle(String title){
+        controller.view.setTitle(title);
+    }
+    
+    public void setAutoAdd(boolean autoAdd){
+        controller.setAutoAdd(autoAdd);
+    }
+    
+    public void setShowRows(boolean showRows) {
+        controller.setShowRows(showRows);
+    }
+    
+    public void setColWidths(int[] width){
+        controller.setColWidths(width);
+    }
+    
+    public void setHeaders(String[] headers){
+        controller.view.setHeaders(headers);
+    }
+    
+    public void setFilterable(boolean[] filterable){
+        controller.setFilterable(filterable);
+    }
+    
+    public void setSortable(boolean[] sort){
+        controller.setSortable(sort);
+    }
+    
+    public void initService(String url) {
+        controller.initService(url);
+    }
+    
+    /**
+     * This method sets the model in the screen. rows = -1 means a serviceUrl
+     * has been provided and the getModel method will be called to populate the
+     * table.
+     * 
+     * @param rows
+     */
+    public void init(int rows) {
+        controller.view.initTable(controller);
+        controller.setView(controller.view);
+        if (rows < 0)
+            controller.getModel();
+        else {
+            for (int m = 0; m < rows; m++) {
+                controller.model.addRow(null);
+            }
+            controller.reset();
+        }
+    }
+}
