17a8f7c28c35a720048db86bec0fc61613f920f3
tbrady <tbrady@uhl.uiowa.edu>
2008-03-31 22:02:45 +0000

    took out the collection boolean because we dont need it if we are using LEFT JOIN

--- src/org/openelis/util/Meta.java
+++ src/org/openelis/util/Meta.java
@@ -12,10 +12,5 @@ public interface Meta {
 	//Jboss will handle this for us if it is mapped in the entity and isnt a collection.
 	public boolean includeInFrom();
 	
-	//this boolean tells the query builder if it is a collection in the entity. If it is then we need
-	//to use the IN() tag so we can access single columns inside a collection.  This is due to limitations of the
-	//EJB query language.
-	public boolean isCollection();
-	
 	public boolean hasColumn(String columnName);
 }
