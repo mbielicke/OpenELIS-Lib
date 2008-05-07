7f4d102ac8c384e9a4d41f135c3c664e9fb8ee67
tbrady <tbrady@uhl.uiowa.edu>
2008-05-07 20:44:59 +0000

    a fix for setting the value to ""

--- src/org/openelis/gwt/common/data/NumberObject.java
+++ src/org/openelis/gwt/common/data/NumberObject.java
@@ -36,7 +36,7 @@ public class NumberObject implements DataObject, Serializable {
     public void setValue(Object object) {
         invalid = false;
         try {
-            if (object != null) {
+            if (object != null && !"".equals(object)) {
                 if (object instanceof String && !((String)object).equals(""))
                     value = Double.valueOf((String)object);
                 else if (object instanceof Double)
