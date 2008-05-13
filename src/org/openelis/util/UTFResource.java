f606b26c4a3625f6df2a66feb62ff5134d07efa6
tbrady <tbrady@uhl.uiowa.edu>
2008-05-13 13:11:22 +0000

    made some changes so that we dont initialize a new resource bundle with every screen

--- src/org/openelis/util/UTFResource.java
+++ src/org/openelis/util/UTFResource.java
@@ -1,5 +1,6 @@
 package org.openelis.util;
 
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
@@ -7,17 +8,36 @@ import java.util.ResourceBundle;
 public class UTFResource {
     
     private PropertyResourceBundle bundle;
+    private static final String OPENELIS_CONSTANTS = "org.openelis.modules.main.server.constants.OpenELISConstants";
+    public static String DEFAULT_LANG = "en";
+    private static HashMap<Locale, UTFResource> cache = new HashMap();
     
-    public static UTFResource getBundle(String name){
-        UTFResource ret = new UTFResource();
-        ret.bundle = (PropertyResourceBundle)ResourceBundle.getBundle(name);
+    public static UTFResource getBundle(String constantClass, Locale locale){
+        UTFResource ret;
+        
+        ret = cache.get(locale);
+        
+        if(ret == null)
+            ret = buildBundle(constantClass, locale);
+            
         return ret;
     }
     
-    public static UTFResource getBundle(String name, Locale locale){
-        UTFResource ret = new UTFResource();
-        ret.bundle = (PropertyResourceBundle)ResourceBundle.getBundle(name,locale);
-        return ret;
+    public static UTFResource getBundle(String localeString){
+        UTFResource res;
+        Locale locale;
+        
+        if(localeString != null)
+            locale = new Locale(localeString);
+        else
+            locale = new Locale(DEFAULT_LANG);            
+        
+        res = cache.get(locale);
+        
+        if(res == null)
+            res = buildBundle(OPENELIS_CONSTANTS, locale);       
+        
+        return res;
     }
     
     public String getString(String key){
@@ -28,5 +48,12 @@ public class UTFResource {
         }
     }
     
-
+    private static UTFResource buildBundle(String name, Locale locale){
+        UTFResource ret = new UTFResource();
+        
+        ret.bundle = (PropertyResourceBundle)ResourceBundle.getBundle(name,locale);
+        cache.put(locale, ret);
+        
+        return ret;
+    }
 }
