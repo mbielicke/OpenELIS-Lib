9661f581497432886a9d7e90e822975b174c2bc9
tbrady <tbrady@uhl.uiowa.edu>
2008-10-08 15:56:55 +0000

    fixes so the new autodropdown call can send error forward

--- src/org/openelis/gwt/common/FormErrorException.java
+++ src/org/openelis/gwt/common/FormErrorException.java
@@ -19,23 +19,11 @@ public class FormErrorException extends RPCException{
 
 	private static final long serialVersionUID = 1L;
 
-	private String msg;
-    public String appMsg;
-
     public FormErrorException() {
         super();
     }
 
     public FormErrorException(String msg) {
         super(msg);
-        this.msg = msg;
-    }
-
-    public String getMessage() {
-        return this.msg;
-    }
-    
-    public void setMessage(String message){
-    	this.msg = message;
     }
 }
