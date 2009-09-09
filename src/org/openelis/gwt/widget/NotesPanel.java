5e3e014f925907c9654d338233b239d80348966b
tbrady <tbrady@uhl.uiowa.edu>
2009-09-09 18:36:39 +0000

    look changes for notes panel

--- src/org/openelis/gwt/widget/NotesPanel.java
+++ src/org/openelis/gwt/widget/NotesPanel.java
@@ -3,8 +3,6 @@ package org.openelis.gwt.widget;
 import org.openelis.gwt.common.Datetime;
 
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HasAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -33,30 +31,24 @@ public class NotesPanel extends Composite {
 		VerticalPanel note = new VerticalPanel();
 		HorizontalPanel topRow = new HorizontalPanel();
 		Label titleText = new Label(subject);
-		HorizontalPanel authorPanel = new HorizontalPanel();
-		Label dateText = new Label(time.toString());
-		Label authorText = new Label(userName);
+		Label userDateText = new Label(userName + " on " + time.toString());
 		Label bodyText = new Label(text);
 		bodyText.setWordWrap(true);
 		note.setWidth(width);
 		topRow.setWidth(width);
 		note.add(titleText);
-		note.add(authorPanel);
+		note.add(userDateText);
 		note.add(bodyText);
-		authorPanel.add(authorText);
-		authorPanel.add(dateText);
-		authorPanel.setWidth("100%");
-		authorPanel.setCellHorizontalAlignment(dateText, HasAlignment.ALIGN_RIGHT);
-		titleText.setStyleName("notSubjectText");
+		titleText.setStyleName("noteSubjectText");
+		userDateText.setStyleName("noteAuthorText");
+		bodyText.setStyleName("noteBodyText");
 		
-		if(notes.getWidgetCount() > 0)
-			notes.add(new HTML("<hr/>"));
 		notes.add(note);
 		
-		if((notes.getWidgetCount()/2) % 2 == 1)
-			note.addStyleName("AltTableRow");
+		if(notes.getWidgetCount() % 2 == 1)
+			note.addStyleName("noteAltTableRow");
 		else
-			note.addStyleName("TableRow");
+			note.addStyleName("noteTableRow");
 		
 	}
 	
