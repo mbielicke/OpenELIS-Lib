61fa327c3c79de12bae609735f347ae207b7bb1e
dshirazi <dari.shirazi@gmail.com>
2009-11-29 23:32:50 +0000

    Removed couple of old interfaces from meta.

--- src/org/openelis/gwt/common/Meta.java
+++ src/org/openelis/gwt/common/Meta.java
@@ -27,9 +27,6 @@ package org.openelis.gwt.common;
 
 public interface Meta {
 
-	public String[] getColumnList();
-	
-	public String getEntity();
-	
 	public boolean hasColumn(String columnName);
+
 }
