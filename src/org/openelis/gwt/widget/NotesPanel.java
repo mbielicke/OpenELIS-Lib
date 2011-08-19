0fd41e92543a6545198b32312445b2ac24f4604d
dshirazi <dari.shirazi@gmail.com>
2011-08-19 14:01:48 +0000

    Changed the note panel to not remove all the line-feeds Changes to CSS for favorites side panel

--- src/org/openelis/gwt/widget/NotesPanel.java
+++ src/org/openelis/gwt/widget/NotesPanel.java
@@ -8,59 +8,66 @@ import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class NotesPanel extends Composite {
-	
-	private ScrollPanel scroll = new ScrollPanel();
-	private VerticalPanel notes = new VerticalPanel();
-	private String width;
-	
-	public NotesPanel() {
-		initWidget(scroll);
-		scroll.setWidget(notes);
-	}
-	
-	public void setWidth(String width) {
-		this.width = width;
-		scroll.setWidth(width);
-	}
-	
-	public void setHeight(String height) {
-		scroll.setHeight(height);
-	}
-	
-	public void addNote(String subject, String userName, String text, Datetime time) {
-		VerticalPanel note = new VerticalPanel();
-		HorizontalPanel topRow = new HorizontalPanel();
-		DateField field = new DateField();
-		field.setBegin(Datetime.YEAR);
-		field.setEnd(Datetime.SECOND);
-		field.setFormat("yyyy-MM-dd HH:mm");
-		field.setValue(time);
-		Label titleText = new Label(subject);
-		Label userDateText = new Label(" by "+userName + " on " + field.format());
-		Label bodyText = new Label(text);
-		bodyText.setWordWrap(true);
-		note.setWidth(width);
-		topRow.setWidth(width);
-		if(subject != null){
-		    note.add(titleText);
-		    note.add(userDateText);
-		}
-		note.add(bodyText);
-		titleText.setStyleName("noteSubjectText");
-		userDateText.setStyleName("noteAuthorText");
-		bodyText.setStyleName("noteBodyText");
-		
-		notes.add(note);
-		
-		if(notes.getWidgetCount() % 2 == 0)
-			note.addStyleName("noteAltTableRow");
-		else
-			note.addStyleName("noteTableRow");
-		
-	}
-	
-	public void clearNotes() {
-		notes.clear();
-	}
+
+    private ScrollPanel   scroll = new ScrollPanel();
+    private VerticalPanel notes  = new VerticalPanel();
+    private String        width;
+
+    public NotesPanel() {
+        initWidget(scroll);
+        scroll.setWidget(notes);
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+        scroll.setWidth(width);
+    }
+
+    public void setHeight(String height) {
+        scroll.setHeight(height);
+    }
+
+    public void addNote(String subject, String userName, String text, Datetime time) {
+        Label subjectText, userDateText, bodyText;
+        VerticalPanel note;
+        HorizontalPanel topRow;
+        DateField field;
+        
+        field = new DateField();
+        field.setBegin(Datetime.YEAR);
+        field.setEnd(Datetime.SECOND);
+        field.setFormat("yyyy-MM-dd HH:mm");
+        field.setValue(time);
+
+        userDateText = new Label(" by " + userName + " on " + field.format());
+        userDateText.setStyleName("noteAuthorText");
+
+        bodyText = new Label(text);
+        bodyText.setWordWrap(true);
+        bodyText.setStyleName("noteBodyText");
+
+        note = new VerticalPanel();
+        note.setWidth(width);
+
+        topRow = new HorizontalPanel();
+        topRow.setWidth(width);
+        if (subject != null) {
+            subjectText = new Label(subject);
+            subjectText.setStyleName("noteSubjectText");
+            note.add(subjectText);
+            note.add(userDateText);
+        }
+        note.add(bodyText);
+
+        notes.add(note);
+        if (notes.getWidgetCount() % 2 == 0)
+            note.addStyleName("noteAltTableRow");
+        else
+            note.addStyleName("noteTableRow");
+    }
+
+    public void clearNotes() {
+        notes.clear();
+    }
 
 }
