10cd6c530049c082324fad6c2fd7efb9214cf0c9
mbielick <mbielicke@users.noreply.github.com>
2012-01-12 16:33:09 +0000

    Cleaned up classpath and unneeded persistence classes.

--- src/org/openelis/persistence/Message.java
+++
@@ -1,9 +0,0 @@
-package org.openelis.persistence;
-
-import java.io.Serializable;
-
-public interface Message extends Serializable {
-    
-    public String getHandler();
-    
-}
--- src/org/openelis/persistence/MessageHandler.java
+++
@@ -1,7 +0,0 @@
-package org.openelis.persistence;
-
-public interface MessageHandler<O extends Message> {
-    
-    public void handle(O message);
-
-}
