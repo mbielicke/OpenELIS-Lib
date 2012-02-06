df119706e3905689b07f1988c7cffe5b8268b2d8
dshirazi <dari.shirazi@gmail.com>
2012-02-06 17:33:42 +0000

    Added a missing lib for fileservlet.

--- src/org/openelis/gwt/server/FileServlet.java
+++ src/org/openelis/gwt/server/FileServlet.java
@@ -14,62 +14,67 @@ import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-public class FileServlet  extends HttpServlet {
+/**
+ * This class provide the parse capability for file upload. This class works
+ * with file upload widget to send files from the client to server.
+ */
 
-	private static final long serialVersionUID = 1L;
+public class FileServlet extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		List<FileItem> files;
-		FileItem file = null;
-		FileItemFactory factory;
-		ServletFileUpload upload;
-		String service = null, method = null;
-		Object serviceInst;
-		
-		try {
-			factory = new DiskFileItemFactory();
+    private static final long serialVersionUID = 1L;
 
-			upload = new ServletFileUpload(factory);
-			
-			files = upload.parseRequest(req);
-			
-			file = files.get(0);
-			
-			for(int i = 1; i < files.size(); i++) {
-				if(files.get(i).getFieldName().equals("service")) 
-					service = files.get(i).getString();
-				else if(files.get(i).getFieldName().equals("method"))
-					method = files.get(i).getString();
-			}
-		}catch(Exception e) {
-			throw (ServletException)e.getCause();
-		}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+                    IOException {
+        List<FileItem> files;
+        FileItem file = null;
+        FileItemFactory factory;
+        ServletFileUpload upload;
+        String service = null, method = null;
+        Object serviceInst;
 
-		Class[] paramTypes = {FileItem.class};
-		Object[] params = {file};
-		try {
-			serviceInst = Class.forName(service).newInstance();
-		    serviceInst.getClass().getMethod(method, paramTypes).invoke(serviceInst, params);
-		
-		} catch(InvocationTargetException e){
-			e.printStackTrace();
-			if(e.getCause() != null)
-				throw new ServletException(e.getCause());
-			else
-				throw new ServletException(e.getTargetException());
-		} catch (NoSuchMethodException e) {
-            throw new ServletException("NoSuchMethodException: "+e.getMessage());
-		} catch(Exception e){
+        try {
+            factory = new DiskFileItemFactory();
+            upload = new ServletFileUpload(factory);
+            files = upload.parseRequest(req);
+
+            /*
+             * File name, service, and method are required from front 
+             */
+            file = files.get(0);
+            for (int i = 1; i < files.size(); i++ ) {
+                if ("service".equals(files.get(i).getFieldName()))
+                    service = files.get(i).getString();
+                else if ("method".equals(files.get(i).getFieldName()))
+                    method = files.get(i).getString();
+            }
+        } catch (Exception e) {
+            throw (ServletException)e.getCause();
+        }
+
+        /*
+         * call the method with the file name parameter for processing
+         */
+        try {
+            serviceInst = Class.forName(service).newInstance();
+            serviceInst.getClass().getMethod(method, new Class[]{FileItem.class}).invoke(serviceInst, new Object[]{file});
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            if (e.getCause() != null)
+                throw new ServletException(e.getCause());
+            else
+                throw new ServletException(e.getTargetException());
+        } catch (NoSuchMethodException e) {
+            throw new ServletException("NoSuchMethodException: " + e.getMessage());
+        } catch (Exception e) {
             e.printStackTrace();
             throw new ServletException(e.getMessage());
         }
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		doGet(req,resp);
-	}
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+                    IOException {
+        doGet(req, resp);
+    }
 }
